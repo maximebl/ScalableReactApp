735dbb1de756af78f924478eec15b404af96b0a6
maximebl <maximebl@videotron.ca>
2017-04-22 16:17:35 -0400

    first commit

---
+++ app/components/Navigation/index.js
@@ -0,0 +1,28 @@
+/**
+*
+* Navigation
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+function Navigation({topics}) {
+  return (
+    <div className={styles.navigation}>
+      we have {topics.length} topics in the nav component
+    </div>
+  );
+}
+
+Navigation.prototype = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Navigation;
